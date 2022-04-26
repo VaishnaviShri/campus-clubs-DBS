b2c5d88140c6a4473b24962e3f5ab65a8650d4f8
Nishith Kumar <73156204+Nis0208@users.noreply.github.com>
2022-04-26 15:00:11 +0530

    fetch update client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React from "react";
+import React, {Fragment} from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import { Routes } from 'react-router-dom';
 import chLanding from './routes/chLanding';
