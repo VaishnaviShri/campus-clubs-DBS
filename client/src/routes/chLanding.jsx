f8bb56923137be10d964aff05200d9ff3fc0760b
Nishith Kumar <73156204+Nis0208@users.noreply.github.com>
2022-04-27 12:44:25 +0530

    landing page ch

--- client/src/routes/chLanding.jsx
+++ client/src/routes/chLanding.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Following_ch from '../components/Following_ch';
 import Header_ch from '../components/Header_ch';
 import Newpost_ch from '../components/newpost_ch';
 import Postlist_ch from '../components/postlist_ch';
@@ -7,15 +8,20 @@ import Postlist_ch from '../components/postlist_ch';
 const chLanding = () => {
   return (
     <div>
+
       <div>
       <Header_ch />
       </div>
+      
       <div>
         <Newpost_ch/>
+        <Following_ch/>
       </div>
+
       <div>
         <Postlist_ch/>
       </div>
+      
     </div>
   )
 }
